b2684409823be00c09e003ee82c20321f56d94ec
Javier M <javier.mgc@outlook.com>
2022-12-28 17:12:13 -0600

    Desencriptar

--- main.js
+++ main.js
@@ -1,67 +1,58 @@
-var textIn = document.querySelector('#text-in');
-var textOut = document.querySelector('#text-out');
+var textIn      = document.querySelector('#text-in');
+var textOut     = document.querySelector('#text-out');
 
 
 
 
 /**
- * Encriptar mensaje
+ * Click en btn "Encriptar"
  */
 function encrypted_btn(){
-    //Mostrar boton de copiar
-    document.querySelector('#btn_copy').classList.remove('ocultar');
-    
-    //=====PROCESO DE ENCRIPTADO
+
     if(!validate_textLength(textIn.value)){
         alert('Campo vacío');
+
+    }else{
+        //Mostrar boton de copiar
+        document.querySelector('#btn_copy').classList.remove('ocultar');
+
+        //Ocultar texto default
+        document.querySelector('#text-out-content__text-default').classList.add('ocultar');
+
+        //Mostrar texto encriptado
+        textOut.innerHTML = encrypt(textIn.value);
     }
-
-    if(validate_textLength(textIn.value)){
-        alert('Chido');
-    }
-
-    
-
-    //=====SALIDA
-
-    //Ocultar texto default
-    document.querySelector('#text-out-content__text-default').classList.add('ocultar');
-
-
-    textOut.innerHTML = textIn.value;
 }
 
 
 
 /**
- * Desencriptar mensaje 
+ * Click btn "Desencriptar"
  */
 function decrypted_btn(){
     document.querySelector('#text-out-content__text-default').classList.add('ocultar');
 
 
-
-
-    
     textOut.innerText = "Menbsaje desencriptado";
 
 }
 
+
+
 /**
- * Copiar mensaje
+ * Click btn "Copiar"
  */
 function copy_btn(){
     textIn.value = textOut.innerText
-
-
-
 }
 
 
+
 /**
- * 
+ * Valida la longitud de texto de entrada input textIn
  * @param {String} text 
- * @returns 
+ * @returns true: Hay almenos una letra en campo de entrada. 
+ *          false: No hay texto en el campo de entrada.
  */
 function validate_textLength(text){
     if(text.length == 0){
@@ -70,4 +61,44 @@ function validate_textLength(text){
     }else{
         return true;
     }
+}
+
+/**
+ * Encripta el mensaje del input de entrada (textIn);
+ * @param {*} text 
+ * @returns {String} 
+ */
+function encrypt(text){
+    let newText ="";
+
+    for(let char of text){
+        //console.log(char);
+
+        switch (char){
+            case "a":
+                newText = newText + "ai";
+                break;
+
+            case "e":
+                newText = newText + "enter";
+                break;
+            
+            case "i":
+                newText = newText + "imes";
+                break;
+
+            case "o":
+                newText = newText + "ober";
+                break;
+
+            case "u":
+                newText = newText + "ufat";
+                break;
+            
+            default:
+                newText = newText + char;
+        }
+    }
+
+    return newText;
 }
